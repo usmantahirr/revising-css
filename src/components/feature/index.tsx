666c42a0c80ff2eb5694f980eddab6186847f3f6
Usman Tahir <hello@usmantahir.com>
2023-04-12 16:00:28 +0200

    WhatGPT part complete

--- src/components/feature/index.tsx
+++ src/components/feature/index.tsx
@@ -1,5 +1,20 @@
-const Feature = () => {
-  return <div>This is Feature</div>;
+import "./styles.css";
+
+type FeatureProps = {
+  title: string;
+  text: string;
 };
+
+const Feature = ({ title, text }: FeatureProps) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
 export default Feature;
